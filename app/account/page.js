419e646486389a24f7b92727e4b69b8771eb5108
omad_d <odilshodvekov13@gmail.com>
2024-08-13 12:16:12 +0500

    Project ready to be deployed

---
+++ app/account/page.js
@@ -0,0 +1,16 @@
+import { auth } from "../_lib/auth"
+
+export const metadata = {
+  title: "Guest area",
+}
+
+export default async function Page() {
+  const session = await auth()
+  const firstName = session.user.name.split(" ").at(0)
+
+  return (
+    <h2 className="text-2xl font-semibold text-accent-400 mb-7">
+      Welcome, {firstName}!
+    </h2>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nrvcsshwaaqdzrsmlgwa.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+  // output: "export",
+}
